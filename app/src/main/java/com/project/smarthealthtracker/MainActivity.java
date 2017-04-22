8397075c7448894b930997f9eb69e6af34a46927
srihaar <krishnasrihaar@gmail.com>
2017-04-21 17:44:30 -0700

    intial code

---
+++ app/src/main/java/com/project/smarthealthtracker/MainActivity.java
@@ -0,0 +1,36 @@
+package com.project.smarthealthtracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loginBtn = (Button) findViewById(R.id.bLoginP);
+        Button registerBtn = (Button) findViewById(R.id.bRegisterP);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
