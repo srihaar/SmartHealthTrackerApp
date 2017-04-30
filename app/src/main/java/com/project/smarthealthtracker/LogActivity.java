85859cf86c182520b3aac436c49b3612a2481b2f
srihaar <krishnasrihaar@gmail.com>
2017-04-30 02:57:51 -0700

    Added check calories functionality

--- app/src/main/java/com/project/smarthealthtracker/LogActivity.java
+++ app/src/main/java/com/project/smarthealthtracker/LogActivity.java
@@ -1,20 +1,129 @@
 package com.project.smarthealthtracker;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.loopj.android.http.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
-public class LogActivity extends AppCompatActivity {
+import cz.msebera.android.httpclient.entity.mime.Header;
+
+public class LogActivity extends AppCompatActivity{
+    Button checkCalories;
+    AsyncHttpClient client;
+    TextView foodName,servings,weight,weightDate;
+    ProgressDialog progressDialog;
+    AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
         getSupportActionBar().setTitle("Log Food and Weight");
+        checkCalories = (Button) findViewById(R.id.getCaloriesBtn);
+        foodName = (TextView) findViewById(R.id.foodName);
+        servings = (TextView) findViewById(R.id.servings);
+        weight = (TextView) findViewById(R.id.weight);
+        weightDate = (TextView) findViewById(R.id.weightDate);
+        client = new AsyncHttpClient();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Getting Calories");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        alertDialog = new AlertDialog.Builder(LogActivity.this).create();
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        checkCalories.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                progressDialog.show();
+                    getNdbno();
+            }
+        });
     }
+
+
+
+    //Get Ndbno
+    public void getNdbno(){
+        RequestParams params = new RequestParams();
+        params.put("format","json");
+        params.put("q",foodName.getText().toString());
+        params.put("api_key","GTHGyVUgYnz3NhhfYIRUIf7WRv11Hxbr5MKulieu");
+        client.get("https://api.nal.usda.gov/ndb/search/", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject obj) {
+                try{
+                    getCalories(obj.getJSONObject("list").getJSONArray("item").getJSONObject(0).getString("ndbno"));
+
+                }catch(Exception e){
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable e , JSONArray a) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                progressDialog.hide();
+                alertDialog.setMessage("Something Went Wrong, Please try again");
+                alertDialog.show();
+            }
+        });
+    }
+
+
+    //Finding calories
+    public void getCalories(String ndbno){
+        RequestParams params = new RequestParams();
+        params.put("format","json");
+        params.put("api_key","GTHGyVUgYnz3NhhfYIRUIf7WRv11Hxbr5MKulieu");
+        params.put("nutrients","208");
+        params.put("ndbno",ndbno);
+        client.get("https://api.nal.usda.gov/ndb/nutrients/", params, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject obj) {
+                try{
+                    JSONObject foodObj = obj.getJSONObject("report").getJSONArray("foods").getJSONObject(0);
+                    JSONObject nutrient = foodObj.getJSONArray("nutrients").getJSONObject(0);
+                    String value = nutrient.getString("value");
+                    progressDialog.hide();
+                    alertDialog.setMessage("Calories per serving is " + value);
+                    alertDialog.show();
+
+                }catch(Exception e){
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable e , JSONArray a) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                progressDialog.hide();
+                alertDialog.setMessage("Something Went Wrong, Please try again");
+                alertDialog.show();
+            }
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
